c5dbda1992b00720233623b52c6eecc9a00573da
DESKTOP-FGFDBVD\leone <leonellopezvazquez2@gmail.com>
2024-06-05 10:40:48 -0500

    start adding some scripts

---
+++ Assets/scripts/Game/Controllers/Gamemenu.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Gamemenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject pauseMenuPanel;
+    bool isPaused = false;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                PauseGame();
+            }
+        }
+    }
+
+    void PauseGame()
+    {
+        pauseMenuPanel.SetActive(true);
+        Time.timeScale = 0f; // Pausar el juego
+        isPaused = true;
+    }
+
+    public void ResumeGame()
+    {
+        pauseMenuPanel.SetActive(false);
+        Time.timeScale = 1f; // Reanudar el juego
+        isPaused = false;
+    }
+
+    public void LoadMainMenu()
+    {
+        Time.timeScale = 1f; // Asegurarse de que el tiempo esté normalizado
+        SceneManager.LoadScene("MenuScene"); // Asegúrate de que la escena del menú principal esté correctamente nombrada
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit(); // Esto solo funciona en una build del juego, no en el editor
+    }
+
+
+}
--- Assets/scripts/menu/MenuController.cs
+++ Assets/scripts/menu/MenuController.cs
@@ -2,12 +2,54 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MenuController : MonoBehaviour
 {
     // Start is called before the first frame update
+    public GameObject panelPrincipal;
+    private void Awake()
+    {
+        GameObject[] panels = GameObject.FindGameObjectsWithTag("UIPanel");
+        foreach (GameObject child in panels)
+        {
+            if (child != panelPrincipal)
+            {
+                child.SetActive(false);
+            }
+            
+        }
+    }
     public void jugar()
     {
-        SceneManager.LoadScene("LobbyScene");
+        SceneManager.LoadScene("GameScene");
     }
+
+    public void opciones()
+    {
+        Debug.Log("entrando al menu opciones");
+    }
+
+    public void atras()
+    {
+        Debug.Log("Hacia atras");
+    }
+
+    public void navigate(GameObject panel)
+    {
+        // hasrc visibles todos los paneles encontradoes en la escena, menos el panel del argumento
+        GameObject[] panels = GameObject.FindGameObjectsWithTag("UIPanel"); 
+        foreach(GameObject child in panels)
+        {
+            if (child != panel)
+            {
+                
+                child.SetActive(false);
+            }
+            
+        }
+        panel.SetActive(true);
+    }
+
 }
+    
